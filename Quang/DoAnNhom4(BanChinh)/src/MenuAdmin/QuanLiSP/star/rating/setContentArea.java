23680bab09974757dd00a32ba94cde094f57900e
hailenquang <161577734+hailenquang@users.noreply.github.com>
2024-04-05 18:43:51 +0700

    dasda
    dsasdcsada

---
+++ Quang/DoAnNhom4(BanChinh)/src/MenuAdmin/QuanLiSP/star/rating/setContentArea.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MenuAdmin.QuanLiSP.star.rating;
+
+/**
+ *
+ * @author Quangratdeptrai
+ */
+ class setContentArea {
+    
+}
