02547302388ff39bc9d3c32cd6934128fc3f3fa5
hailenquang <161577734+hailenquang@users.noreply.github.com>
2024-04-22 12:41:12 +0700

    quang nop bai 22/4/2022

---
+++ quang nhom/build/classes/MenuAdmin/QuanLiSP/rating/StarRating.rs
@@ -0,0 +1,6 @@
+rating.StarRating
+rating.StarRating$4
+rating.StarRating$5
+rating.StarRating$2
+rating.StarRating$3
+rating.StarRating$1
---
+++ quang nhom/src/MenuAdmin/QuanLiSP/ClikeThemSp/Danhsach1.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MenuAdmin.QuanLiSP.ClikeThemSp;
+
+import MenuAdmin.QuanLiSP.*;
+
+/**
+ *
+ * @author Quangratdeptrai
+ */
+public class Danhsach1 {
+    private String Hang;
+    private int Gia;
+    private String dt;
+    private String nc;
+    private String dl;
+    private String tn;
+    private String tndb;
+    private String ram;
+    private String FileAvata;
+    
+
+    public String getHang() {
+        return Hang;
+    }
+
+    public int getGia() {
+        return Gia;
+    }
+
+    public String getDt() {
+        return dt;
+    }
+
+    public String getNc() {
+        return nc;
+    }
+
+    public String getDl() {
+        return dl;
+    }
+
+    public String getTn() {
+        return tn;
+    }
+
+    public String getTndb() {
+        return tndb;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public String getFileAvata() {
+        return FileAvata;
+    }
+
+    public void setHang(String Hang) {
+        this.Hang = Hang;
+    }
+
+    public void setGia(int Gia) {
+        this.Gia = Gia;
+    }
+
+    public void setDt(String dt) {
+        this.dt = dt;
+    }
+
+    public void setNc(String nc) {
+        this.nc = nc;
+    }
+
+    public void setDl(String dl) {
+        this.dl = dl;
+    }
+
+    public void setTn(String tn) {
+        this.tn = tn;
+    }
+
+    public void setTndb(String tndb) {
+        this.tndb = tndb;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+     public void setFileAvata(String anh) {
+        this.FileAvata = FileAvata;
+    }
+
+    public Danhsach1(String Hang, String dt, String nc, String dl, String tn, String tndb, String ram, int Gia, String FileAvata) {
+        this.Hang = Hang;
+        this.Gia = Gia;
+        this.dt = dt;
+        this.nc = nc;
+        this.dl = dl;
+        this.tn = tn;
+        this.tndb = tndb;
+        this.ram = ram;
+        this.FileAvata = FileAvata;
+    }
+}
