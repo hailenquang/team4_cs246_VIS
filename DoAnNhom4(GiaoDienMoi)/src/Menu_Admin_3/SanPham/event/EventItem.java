9431288163d7259002bc8e37c3241f0437006c59
ThanhAn1002 <161575003+ThanhAn1002@users.noreply.github.com>
2024-05-05 11:11:56 +0700

    xoa

--- DoAnNhom4(GiaoDienMoi)/src/Menu_Admin_3/SanPham/event/EventItem.java
+++
@@ -1,9 +0,0 @@
-package Menu_Admin_3.SanPham.event;
-
-import Menu_Admin_3.SanPham.model.ModelItem;
-import java.awt.Component;
-
-public interface EventItem {
-
-    public void itemClick(Component com, ModelItem item);
-}
